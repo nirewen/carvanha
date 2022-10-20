5c5dfd36fc6f3867f23818990c600e6e27f0e339
Eduardo Londero <londero.edu@gmail.com>
2022-10-20 19:18:05 +0000

    Generate from Maven archetype

---
+++ src/main/java/br/ufsm/csi/redes/App.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.redes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
