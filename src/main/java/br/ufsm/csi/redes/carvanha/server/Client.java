fdca05858da026d4bb656dcf371cb3486d42738a
Eduardo Londero <londero.edu@gmail.com>
2022-10-21 03:34:56 +0000

    Save hostname into a variable | Hide prompt

--- src/main/java/br/ufsm/csi/redes/carvanha/server/Client.java
+++ src/main/java/br/ufsm/csi/redes/carvanha/server/Client.java
@@ -5,7 +5,6 @@ import java.net.InetAddress;
 import java.util.Scanner;
 
 import br.ufsm.csi.redes.carvanha.model.Payload;
-import br.ufsm.csi.redes.carvanha.model.Response;
 import br.ufsm.csi.redes.carvanha.model.Payload.Type;
 import br.ufsm.csi.redes.carvanha.util.PacketSender;
 import lombok.SneakyThrows;
@@ -15,25 +14,23 @@ public class Client implements Runnable {
     @SneakyThrows
     public void run() {
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter hostname: ");
+            String hostname = scanner.nextLine();
+
             while (true) {
                 try (DatagramSocket socket = new DatagramSocket()) {
-                    System.out.print("Enter hostname: ");
-                    String hostname = scanner.nextLine();
+                    String message = scanner.nextLine();
 
                     Payload payload = Payload
                             .builder()
-                            .type(Type.PING)
-                            .name("Eduardo")
+                            .type(Type.MESSAGE)
                             .time(System.nanoTime())
+                            .content(message)
                             .build();
 
                     PacketSender sender = new PacketSender(socket);
 
                     sender.send(payload, InetAddress.getByName(hostname));
-
-                    Response received = sender.receive();
-
-                    System.out.println(received.data.getName());
                 }
             }
         }
